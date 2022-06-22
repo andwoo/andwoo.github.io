91de1913b4765ec1cbabafa7c036a317b0d1c5bd
Andrew M <andrewlmanley@gmail.com>
2022-06-22 16:40:36 -0400

    Another attempt at fixing autoplay issues on mobile

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -26,14 +26,10 @@ function setup() {
     const fontScale = (asciiContainer.clientWidth / p5VideoWidth) * 1.7
     asciiContainer.style['fontSize'] = `${asciiOptions.blockResolution * fontScale}px`
     asciiContainer.style['lineHeight'] = `${(asciiOptions.blockResolution / 1.2) * fontScale}px`
-
-    p5Video.volume(0);
-    p5Video.elt.muted = true
-    p5Video.play()
   })
-  p5Video.autoplay(false);
+  p5Video.autoplay(true);
+  p5Video.elt.setAttribute('muted', true);
   p5Video.volume(0);
-  p5Video.elt.muted = true
   p5Video.loop();
   p5Video.hide()
 }
